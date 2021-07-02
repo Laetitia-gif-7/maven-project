cf688220fe9e3b6b93816c7f2337788849184490
Laetitia-gif-7 <80823723+Laetitia-gif-7@users.noreply.github.com>
2021-07-02 15:26:49 +0200

    Update Greeter.java
    Ajout des commentaires obligatoires

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,5 +1,11 @@
+/**
+* Javadoc du package.
+*/
 package com.example;
 
+/**
+* This is a class.
+*/
 
 public class Greeter {
 
@@ -7,6 +13,11 @@ public class Greeter {
   public Greeter() {
 
   }
+  
+  /**
+  * @param someone name of a person
+  * @return greeting string
+  */
 
   public final String greet(final String someone) {
     return String.format("Hello babacar, %s!", someone);
