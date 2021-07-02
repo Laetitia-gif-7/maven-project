5296dc2df6fe963a1d07aef0ab3b635fbc4cfc1f
Laetitia-gif-7 <80823723+Laetitia-gif-7@users.noreply.github.com>
2021-07-02 15:36:50 +0200

    Update Greeter.java
    n°3

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -6,13 +6,11 @@ package com.example;
 /**
 * This is a class.
 */
-
 public class Greeter {
   
   /**
   * This is a constructor.
   */
-
   public Greeter() {
 
   }
@@ -21,7 +19,6 @@ public class Greeter {
   * @param someone name of a person
   * @return greeting string
   */
-
   public final String greet(final String someone) {
     return String.format("Hello babacar, %s!", someone);
   }
